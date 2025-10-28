de907eb94920dd691bf5e9180bf7207674365ad6
An Khang <nguyenankhangpro7895123@gmail.com>
2025-10-28 20:58:01 +0700

    Update v.16

--- config/settings.py
+++ config/settings.py
@@ -57,9 +57,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Các thiết lập tùy chọn của allauth
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = False
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_LOGIN_METHODS = {'email'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = 'none'  # Có thể là 'mandatory', 'optional', hoặc 'none'
 SOCIALACCOUNT_LOGIN_ON_GET = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
